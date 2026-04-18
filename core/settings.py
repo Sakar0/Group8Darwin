847dfd58c6c0c8a9e28f52930a44c8c85e37068a
ARYAN303-R <S385826@students.cdu.edu.au>
2026-04-18 20:45:06 +0930

    Add allowed hosts and secret key configuration

--- core/settings.py
+++ core/settings.py
@@ -93,3 +93,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app', '.up.railway.app']
+SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
